05c069f79ceccd086658191cdc0000e4fdcf4ad5
jsosa26 <jesus.asm26@gmail.com>
2022-12-28 23:48:02 +0000

    Primera parte medio lista

--- src/js/app.js
+++ src/js/app.js
@@ -33,16 +33,9 @@ function render(variables = {}) {
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
-          </ul>
-        </div>
+          <h1>${variables.name == null ? "Daniela" : variables.name} ${
+    variables.lastname == null ? "Diaz" : variables.lastname
+  }</h1>
     `;
 }
 
